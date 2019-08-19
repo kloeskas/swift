8af7af837b367ff90ea9ee2517d8ddc125268cc3
Evgeniia Pak <4707071@mail.ru>
2019-08-19 19:13:41 +0300

    добавление попытка 3

---
+++ 3l_PakEvgeniia.playground/Contents.swift
@@ -0,0 +1,133 @@
+import Foundation
+
+enum engineState {
+    case start, stop
+}
+
+enum windowState {
+    case open, close
+}
+
+enum trunkState {
+    case full, empty
+}
+
+struct someCar {
+    let brand : String
+    let model : String
+    var color : String
+    mutating func changeColor(c:String) {
+        switch c {
+        case "white":
+            self.color = "white"
+        case "black":
+            self.color = "black"
+        case "red":
+            self.color = "red"
+        case "blue":
+            self.color = "blue"
+        default:
+            print("Input error.")
+        }
+    }
+    let release : Int
+    var trunkVolume : Double {
+        willSet {
+            if (trunkState == .empty) && (trunkVolume > 0) && (trunkVolume != 0) && (newValue < trunkVolume) {
+                let space = trunkVolume - newValue
+                print ("\(brand)\(model) trunk free: \(space)")
+            } else { print("Input error or \(brand)\(model) trunk is full.")}
+        }
+    }
+    var engineState : engineState {
+        willSet {
+            if newValue == .start {
+                print ("\(brand)\(model) engine is on")
+            } else {print("\(brand)\(model) engine is off")}
+        }
+    }
+    var windowState : windowState {
+        willSet {
+            if newValue == .open {
+                print("\(brand)\(model) windows are open")
+            } else { print("\(brand)\(model) windows are close") }
+        }
+    }
+    var trunkState : trunkState
+    mutating func emptyTrunck() {
+        self.trunkState = .empty
+        print ("\(brand)\(model) trunck is empty")
+    }
+}
+
+struct someTruck {
+    let brand : String
+    let model : String
+    var color : String
+    mutating func changeColor(c:String) {
+        switch c {
+        case "white":
+            self.color = "white"
+        case "black":
+            self.color = "black"
+        case "red":
+            self.color = "red"
+        case "blue":
+            self.color = "blue"
+        default:
+            print("Input error.")
+        }
+    }
+    let release : Int
+    var bodyVolume : Double {
+        willSet {
+            if (trunkState == .empty) && (bodyVolume > 0) && (bodyVolume != 0) && (newValue < bodyVolume) {
+                let space = bodyVolume - newValue
+                print ("\(brand)\(model) trunk free: \(space)")
+            } else { print("Input error or \(brand)\(model) trunk is full.")}
+        }
+    }
+    var engineState : engineState {
+        willSet {
+            if newValue == .start {
+                print ("\(brand)\(model) engine is on")
+            } else {print("\(brand)\(model) engine is off")}
+        }
+    }
+    var windowState : windowState {
+        willSet {
+            if newValue == .open {
+                print("\(brand)\(model) windows are open")
+            } else { print("\(brand)\(model) windows are close") }
+        }
+    }
+    var trunkState : trunkState
+    mutating func emptyTrunck() {
+        self.trunkState = .empty
+        print ("\(brand)\(model) trunck is empty")
+    }
+}
+
+var car1 = someCar(brand: "Porsche", model: "Cayenne", color: "clear", release: 2019, trunkVolume: 590.0 , engineState: .stop, windowState: .open, trunkState: .empty)
+var car2 = someCar(brand: "Lamborghini", model: "Urus", color: "clear", release: 2018, trunkVolume: 900.0, engineState: .stop, windowState: .close, trunkState: .full)
+
+var truck1 = someTruck(brand: "Mercedes-Benz", model: "Actros", color: "clear", release: 2014, bodyVolume: 100000.0, engineState: .start, windowState: .open, trunkState: .full)
+var truck2 = someTruck(brand: "Volvo", model: "FH-13", color: "clear", release: 2014, bodyVolume: 150000.0, engineState: .start, windowState: .close, trunkState: .empty)
+
+
+car1.engineState = .start
+car1.trunkVolume = 540.0
+car1.changeColor(c: "red")
+car2.trunkVolume = 890.0
+car2.emptyTrunck()
+car2.trunkVolume = 80.9
+car2.windowState = .open
+car2.changeColor(c: "black")
+
+truck1.engineState = .stop
+truck1.windowState = .close
+truck2.engineState = .stop
+truck2.bodyVolume = 5578908
+
+
+print ("Information about the first car: brandmodel: \(car1.brand)\(car1.model), release: \(car1.release), color: \(String(describing: car1.color)), free trunkVolume: \(car1.trunkVolume), engine is \(car1.engineState)")
